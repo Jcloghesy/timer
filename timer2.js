846e060dfdb2721129e88f97ae043d9a64dd9255
John <john.cloghesy@gmail.com>
2023-06-05 12:43:52 -0600

    Implement timer 2 function

---
+++ timer2.js
@@ -0,0 +1,20 @@
+// ---------------------   TIMER - 2  --------------------------------------- //
+
+const stdin = process.stdin;
+
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+  key = Number(key);
+  if (Number.isFinite(key) && key >= 0) {
+    console.log("setting timer for " + key + (key === 1? " second." : " seconds."));
+    setTimeout(() => { process.stdout.write('\x07'); }, key * 1000);
+  }
+});
+
